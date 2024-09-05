a28c31b409fb7f938941bcd907eab7ce5537882b
Carson Hellman <carsonie24@gmail.com>
2024-09-04 23:35:27 -0600

    can only put down when within number rolled. Need to make turn based now

--- Assets/Scripts/DominoSelect.cs
+++ Assets/Scripts/DominoSelect.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
 public class DominoSelect : MonoBehaviour
 {
+
+    public GameObject CanvasObject;
+    private Score scoreScript;
+    [SerializeField]
+    TextMeshProUGUI UsablePointsText;
+    public int tileCost;
+    private bool movable;
+    private bool upright = true;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        scoreScript = CanvasObject.GetComponent<Score>();
+        movable = true;
     }
 
     // Update is called once per frame
@@ -19,16 +31,24 @@ public class DominoSelect : MonoBehaviour
 
     public void SelectedDomino()
     {
-        // Debug.Log($"is this: {}");
-        if(transform.eulerAngles.x == 315)
+        scoreScript.rolling = false;
+        if(transform.eulerAngles.x == 315 && (scoreScript.NumRolled - tileCost >= 0))
         {
             // Debug.Log("rotate down");
+            upright = false;
             transform.Rotate(90, 0, 0);
+            scoreScript.NumRolled -= tileCost;
+            UsablePointsText.text = $"{scoreScript.NumRolled} Usable Points";
+            // AT END OF ROUND IF PUT DOWN NEED TO MAKE SET MOVABLE FALSE !!!!!!!!!!!!!!!!!!!
+            // NEED BUTTON TO FINISH SELECTION THEN CHANGE ROLLING BACK TO TRUE ON SCORE SCRIP!!!!
         }
-        else
+        else if (movable && !upright)
         {
-            // Debug.Log("Should rotate back");
+            // Debug.Log($"Should rotate back {tileCost}");
+            upright = true;
             transform.Rotate(270, 0, 0);
+            scoreScript.NumRolled += tileCost;
+            UsablePointsText.text = $"{scoreScript.NumRolled} Usable Points";
         }
     }
 }
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,19 +8,25 @@ public class Score : MonoBehaviour
 {
     public DiceRoll dice1;
     public DiceRoll dice2;
+    public int NumRolled;
+    public bool rolling = true;
 
     [SerializeField]
     TextMeshProUGUI scoreText;
 
     private void Update()
     {
-        if (dice1 != null && dice2 != null)
+        if(rolling)
         {
-            if (dice1.diceFaceNum != 0 && dice2.diceFaceNum != 0)
+            if (dice1 != null && dice2 != null)
             {
-                if (dice1.GetComponent<Rigidbody>().velocity == Vector3.zero && dice2.GetComponent<Rigidbody>().velocity == Vector3.zero)
+                if (dice1.diceFaceNum != 0 && dice2.diceFaceNum != 0)
                 {
-                    scoreText.text = "You rolled " + (dice1.diceFaceNum + dice2.diceFaceNum).ToString();
+                    if (dice1.GetComponent<Rigidbody>().velocity == Vector3.zero && dice2.GetComponent<Rigidbody>().velocity == Vector3.zero)
+                    {
+                        NumRolled = dice1.diceFaceNum + dice2.diceFaceNum;
+                        scoreText.text = "Rolled " + (NumRolled).ToString();
+                    }
                 }
             }
         }
